030ba0ba73dcc3c369872404e54228076025a329
Thamer <tha-mer@hotmail.com>
2025-03-17 02:43:31 -0700

    Added more plots and filters. Cleaned up layout. Added video demo
    Added more plots and filters. Cleaned up layout. Added video demo

--- src/app.R
+++ src/app.R
@@ -1,6 +1,5 @@
 library(shiny)
 library(bslib)
-library(ggplot2)
 library(dplyr)
 library(readr)
 library(plotly)
@@ -21,50 +20,73 @@ ui <- fluidPage(
         .container-fluid {
             padding: 60px;
         }
+        .fixed-sidebar {
+            position: fixed;
+            top: 180px;
+            width: 1500px;
+        }
+        .main-panel {
+            margin-left: 20%;
+        }
     ")),
     
     # Application title
     titlePanel("What to play? 🎮"),
-    p("Can't decide what game you want to play? Here is dashboard of Steam's 1000 most popular games to help you!"),
+    p("Can't decide what game you want to play? Here is dashboard of Steam's 1000 most popular games to help you! Created by Thamer Aldawood."),
 
     # Sidebar with date range input for filtering games by release date
-    sidebarLayout(
-        sidebarPanel(
-            dateRangeInput("dateRange",
-                           "Select release date range:",
-                           start = min(games$release_date, na.rm = TRUE),
-                           end = max(games$release_date, na.rm = TRUE)),
-            selectInput("genreInput",
-                        "Select genres:",
-                        choices = unique(games$genre),
-                        selected = unique(games$genre),
-                        multiple = TRUE),
-            numericInput("minPlayerScore",
-                         "Minimum player score:",
-                         value = 0,
-                         min = 0,
-                         max = 100),
-            numericInput("minCriticScore",
-                         "Minimum critic score:",
-                         value = 0,
-                         min = 0,
-                         max = 100)
+    fluidRow(
+        column(3,
+               div(class = "fixed-sidebar",
+                   sidebarPanel(
+                       dateRangeInput("dateRange",
+                                      "Select release date range:",
+                                      start = min(games$release_date, na.rm = TRUE),
+                                      end = max(games$release_date, na.rm = TRUE)),
+                       selectInput("genreInput",
+                                   "Select genres:",
+                                   choices = unique(games$genre),
+                                   # selected = unique(games$genre),
+                                   multiple = TRUE),
+                       numericInput("minPlayerScore",
+                                    "Minimum player score:",
+                                    value = 0,
+                                    min = 0,
+                                    max = 100),
+                       numericInput("minCriticScore",
+                                    "Minimum critic score:",
+                                    value = 0,
+                                    min = 0,
+                                    max = 100),
+                       selectInput("scoreType",
+                                   "Select score type for line chart:",
+                                   choices = c("Critic Score" = "metacritic_score", "Player Score" = "pct_pos_total"),
+                                   selected = "metacritic_score")
+                   )
+               )
         ),
-
-        # Show plots of the generated distributions
-        mainPanel(
-            div(class = "card",
-                div(class = "card-body",
-                    h4(class = "text-center", "Scatter Plot of Player Score vs Critic Score"),
-                    plotlyOutput("scatterPlot")
-                )
-            ),
-            div(class = "card",
-                div(class = "card-body",
-                    h4(class = "text-center", "Count of Games per Genre"),
-                    plotlyOutput("genreCountPlot")
-                )
-            )
+        column(9,
+               div(class = "main-panel",
+                   div(class = "card",
+                       div(class = "card-body",
+                           h4(class = "text-center", "Scatter Plot of Player Score vs Critic Score"),
+                           p(class = "text-center", "*point size represents game popularity"),
+                           plotlyOutput("scatterPlot")
+                       )
+                   ),
+                   div(class = "card",
+                       div(class = "card-body",
+                           h4(class = "text-center", "Count of Games per Genre"),
+                           plotlyOutput("genreCountPlot")
+                       )
+                   ),
+                   div(class = "card",
+                       div(class = "card-body",
+                           h4(class = "text-center", "Game Popularity vs Score"),
+                           plotlyOutput("lineChart")
+                       )
+                   )
+               )
         )
     )
 )
@@ -76,7 +98,7 @@ server <- function(input, output) {
         # Filter games by selected date range, genres, and scores
         filtered_games <- games |>
             filter(release_date >= input$dateRange[1] & release_date <= input$dateRange[2]) |>
-            filter(genre %in% input$genreInput) |>
+            filter(if (length(input$genreInput) > 0) genre %in% input$genreInput else TRUE) |>
             filter(pct_pos_total >= input$minPlayerScore) |>
             filter(metacritic_score >= input$minCriticScore)
 
@@ -116,7 +138,7 @@ server <- function(input, output) {
         # Count games per genre
         genre_counts <- games |>
             filter(release_date >= input$dateRange[1] & release_date <= input$dateRange[2]) |>
-            filter(genre %in% input$genreInput) |>
+            filter(if (length(input$genreInput) > 0) genre %in% input$genreInput else TRUE) |>
             filter(pct_pos_total >= input$minPlayerScore) |>
             filter(metacritic_score >= input$minCriticScore) |>
             count(genre) |>
@@ -136,6 +158,40 @@ server <- function(input, output) {
             title = "",
             xaxis = list(title = ""),
             yaxis = list(title = "")
+        )
+    })
+
+    output$lineChart <- renderPlotly({
+        # Filter games by selected date range, genres, and scores
+        filtered_games <- games |>
+            filter(release_date >= input$dateRange[1] & release_date <= input$dateRange[2]) |>
+            filter(if (length(input$genreInput) > 0) genre %in% input$genreInput else TRUE) |>
+            filter(pct_pos_total >= input$minPlayerScore) |>
+            filter(metacritic_score >= input$minCriticScore)
+
+        # Calculate average player count for each score
+        avg_player_count <- filtered_games |>
+            group_by(score = get(input$scoreType)) |>
+            summarize(avg_peak_ccu = mean(peak_ccu, na.rm = TRUE)) |>
+            arrange(score)
+
+        # Create the line chart with average player count
+        plot_ly(
+            data = avg_player_count,
+            x = ~score,
+            y = ~avg_peak_ccu,
+            type = 'scatter',
+            mode = 'lines+markers',
+            text = ~paste(
+                "Score: ", score, "<br>",
+                "Average Peak Player Count: ", avg_peak_ccu
+            ),
+            hoverinfo = 'text'
+        ) |>
+        layout(
+            xaxis = list(title = ifelse(input$scoreType == "metacritic_score", "Critic Score", "Player Score")),
+            yaxis = list(title = "Average Peak Player Count"),
+            legend = list(orientation = 'v', x = -0.35, y = 1)
         )
     })
 }
